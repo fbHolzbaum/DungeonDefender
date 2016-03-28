739522bc495e6d2efc4aa6ecaa7e15a7fcbf13b9
fbHolzbaum <blancofel22@gmail.com>
2016-03-28 23:30:20 +0200

    Started Project
    Included Enemy Movement
    Included Enemy Spawning

---
+++ DungeonDefender/Assets/EnemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed = 1;
+    private Rigidbody player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        //Player movement
+        player.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+
+        //Raycast
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 0.5f))
+        {
+            if(hit.transform.CompareTag("Wall"))
+            {
+                Debug.Log("Wand!");
+                //Check if left way free
+                if(!Physics.Raycast(transform.position, -transform.right, out hit, 1))
+                {
+                    Debug.Log("Turn left!");
+                    player.transform.eulerAngles += new Vector3(0, -90, 0);
+                }
+                else
+                {
+                    Debug.Log("Move Right!");
+                    player.transform.eulerAngles += new Vector3(0, +90, 0);
+                }
+            }
+        } 
+	}
+}
---
+++ DungeonDefender/Assets/SpawnEnemies.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour {
+
+    public Transform spawnPoint;
+    public GameObject[] enemies;
+
+    private float spawnTime = 2;
+    private bool spawnOn = true;
+	
+	// Update is called once per frame
+	void Update () {
+	    if(spawnOn)
+        {
+            Instantiate(enemies[0], spawnPoint.position, spawnPoint.rotation);
+            spawnOn = false;
+            StartCoroutine(CountDown(spawnTime));
+
+        }
+	}
+
+    IEnumerator CountDown(float spawnTime)
+    {
+
+        yield return new WaitForSeconds(spawnTime);
+        spawnOn = true;
+    }
+}
